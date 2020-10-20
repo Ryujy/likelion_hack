9ed12191afe0ed264140a37d07cd59557d727e58
jungeun919 <renee8369@likelion.org>
2020-10-20 14:57:07 +0900

    여성의류 모델 내용 추가

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+# 여성의류
+class Cloth_female(models.Model):
+    photo = models.ImageField(null=True)
+    name = models.CharField(max_length=30)
+    brand = models.CharField(max_length=30)
+    price = models.CharField(max_length=15)
+    color = models.CharField(max_length=15)
+    size = models.CharField(max_length=15)
+
+    # def __str__(self):
+    #     return name
+
+# 남성의류
+class Cloth_male(models.Model):
+    photo = models.ImageField(null=True)
+    name = models.CharField(max_length=30)
+    brand = models.CharField(max_length=30)
+    price = models.CharField(max_length=15)
+    color = models.CharField(max_length=15)
+    size = models.CharField(max_length=15)
+
+    # def __str__(self):
+    #     return name

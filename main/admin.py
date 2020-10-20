28e7fde682c759469fb9d6eb306f9afd2e581dfc
saurav <tsaurav18@gmail.com>
2020-10-20 15:50:44 +0900

    fixing model

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-
 admin.site.register(Cloth_female)
 admin.site.register(Cloth_male)
--- main/models.py
+++ main/models.py
@@ -1,28 +1,38 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 # 여성의류
 class Cloth_female(models.Model):
-    photo = models.ImageField(null=True)
-    name = models.CharField(max_length=30)
+    photo = models.ImageField(null=True, upload_to='media/')
+    name = models.CharField(max_length=100)
     brand = models.CharField(max_length=30)
-    price = models.CharField(max_length=15)
+    price = models.IntegerField(default=0)
     color = models.CharField(max_length=15)
     size = models.CharField(max_length=15)
-
-    # def __str__(self):
-    #     return name
+    
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        managed = False    
 
 # 남성의류
 class Cloth_male(models.Model):
-    photo = models.ImageField(null=True)
-    name = models.CharField(max_length=30)
+    photo = models.ImageField(null=True, upload_to='media/')
+    name = models.CharField(max_length=100)
     brand = models.CharField(max_length=30)
-    price = models.CharField(max_length=15)
+    price = models.IntegerField(default=0)
     color = models.CharField(max_length=15)
     size = models.CharField(max_length=15)
+    
+   
 
-    # def __str__(self):
-    #     return name
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        managed = False    
+
+
     

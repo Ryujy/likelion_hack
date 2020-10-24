537196ba7bfcf04ef71409989bb211739278027f
jungeun919 <renee8369@likelion.org>
2020-10-24 16:09:13 +0900

    모델정의, 온도필터

--- main/models.py
+++ main/models.py
@@ -2,27 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-# 여성의류
-class Cloth_female(models.Model):
-    photo = models.ImageField(null=True)
-    name = models.CharField(max_length=30)
-    brand = models.CharField(max_length=30)
-    price = models.CharField(max_length=15)
-    color = models.CharField(max_length=15)
-    size = models.CharField(max_length=15)
+class Temp_category(models.Model):
+    temp_name = models.CharField(max_length=200)
+    max_temp = models.FloatField()
+    min_temp = models.FloatField()
 
-    # def __str__(self):
-    #     return name
+    def __str__(self):
+        return self.temp_name
+# 의류
+class Cloth(models.Model):
+    photo = models.ImageField(null=True)        # 사진
+    name = models.CharField(max_length=50)      # 옷이름
+    brand = models.CharField(max_length=30)     # 브랜드이름
+    price = models.CharField(max_length=15)     # 가격
+    color = models.CharField(max_length=15)     # 색상
+    size = models.CharField(max_length=15)      # 사이즈
+    gender = models.CharField(max_length = 15)  # 성별
+    sort = models.CharField(max_length=15)      # 상의인지 하의인지
+    temp = models.ForeignKey(Temp_category, on_delete=models.CASCADE, db_column='temp_name')   # 온도 연결
 
-# 남성의류
-class Cloth_male(models.Model):
-    photo = models.ImageField(null=True)
-    name = models.CharField(max_length=30)
-    brand = models.CharField(max_length=30)
-    price = models.CharField(max_length=15)
-    color = models.CharField(max_length=15)
-    size = models.CharField(max_length=15)
-
-    # def __str__(self):
-    #     return name
+    def __str__(self):
+        return self.name
     

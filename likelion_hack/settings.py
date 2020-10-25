560bdae9893017d1b8e09f89bdeaf68ccf5410b8
Ryujy <jiyun3016@likelion.org>
2020-10-25 10:51:09 +0900

    ㅎㅎ

--- likelion_hack/settings.py
+++ likelion_hack/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'likelion_hack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['main/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
